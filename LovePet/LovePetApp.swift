2f758ba73bf3720d9ea4ac9ef71c7ca78d2630f7
NicoleRöhm <donjay1014@gmail.com>
2023-06-23 15:44:30 +0200

    Initial Commit

---
+++ LovePet/LovePetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovePetApp.swift
+//  LovePet
+//
+//  Created by Nicole Röhm on 23.06.23.
+//
+
+import SwiftUI
+
+@main
+struct LovePetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
