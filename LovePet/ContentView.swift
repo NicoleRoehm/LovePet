008cc809de97321a8f25506c781e2b305eddaf26
NicoleRöhm <donjay1014@gmail.com>
2023-06-23 15:46:14 +0200

    myAppresults

--- LovePet/ContentView.swift
+++ LovePet/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            Text(" Hier ensteht ein neues Projekt :P ")
         }
         .padding()
     }
