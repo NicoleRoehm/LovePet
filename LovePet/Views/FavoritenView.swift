aba685a009fd8db18688d307ce24fa4268fe724c
NicoleRöhm <donjay1014@gmail.com>
2023-08-14 13:30:01 +0200

    myAppResults

---
+++ LovePet/Views/FavoritenView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritenView.swift
+//  LovePet
+//
+//  Created by Nicole Röhm on 11.08.23.
+//
+
+import SwiftUI
+
+struct FavoritenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritenView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritenView()
+    }
+}
